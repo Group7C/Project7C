a2e31845f6b3f8c1e8dedb468da5fa87aa193c5c
JibPython <jibrail.ali.2005@gmail.com>
2024-12-14 09:44:46 +0000

    functionality of login working need to be able to change state of screen and need to re-design ui

--- flutter_project/lib/login_screen.dart
+++ flutter_project/lib/login_screen.dart
@@ -9,12 +9,39 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.sizeOf(context);
+    final double width = size.width;
+    final double height = size.height;
+
+    TextEditingController username = TextEditingController();
+    TextEditingController password = TextEditingController();
+
     return Scaffold(
-      body: Container(
-        child: Text("test"),
-      ),
+      body: Row(children: [
+        // first half of the login screen DECORATION
+        SizedBox(height: height, width: width * 0.5, child: Container(color: Colors.blue,),),
+        // second half of the login screen USER DETAILS
+        SizedBox(
+          height: height,
+          width: width * 0.5,
+          child: Column(
+            children: [
+              Text("Login"),
+              const Text("username"),
+              TextFormField(controller: username,),
+              const Text("password"),
+              TextFormField(controller: password,),
+              ElevatedButton(onPressed: () {
+                print(username.text);
+                print(password.text);
+                },
+                  child: Text("confirm"))
+            ],
+        ),)
+      ],),
     );
   }
 }
