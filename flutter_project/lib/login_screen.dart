9e27fdc7e17fb8d891d83d9531512efc34efbbf1
JibPython <jibrail.ali.2005@gmail.com>
2024-12-14 10:21:27 +0000

    cleaning up the ui on the login screen, not finished

--- flutter_project/lib/login_screen.dart
+++ flutter_project/lib/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -9,7 +8,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.sizeOf(context);
@@ -19,29 +17,80 @@ class _LoginScreenState extends State<LoginScreen> {
     TextEditingController username = TextEditingController();
     TextEditingController password = TextEditingController();
 
+    String mode = "Login";
+
+    String getButtonText() {
+      if (mode == "Login") {
+        return "Switch to Sign Up";
+      }
+      return "Switch to Login";
+    }
+
     return Scaffold(
-      body: Row(children: [
-        // first half of the login screen DECORATION
-        SizedBox(height: height, width: width * 0.5, child: Container(color: Colors.blue,),),
-        // second half of the login screen USER DETAILS
-        SizedBox(
-          height: height,
-          width: width * 0.5,
-          child: Column(
-            children: [
-              Text("Login"),
-              const Text("username"),
-              TextFormField(controller: username,),
-              const Text("password"),
-              TextFormField(controller: password,),
-              ElevatedButton(onPressed: () {
-                print(username.text);
-                print(password.text);
-                },
-                  child: Text("confirm"))
-            ],
-        ),)
-      ],),
+      body: Row(
+        children: [
+          // first half of the login screen DECORATION
+          SizedBox(
+            height: height,
+            width: width * 0.5,
+            child: Container(
+
+                color: Colors.blue,
+                child: const Center(child: Text("Welcome!", style: TextStyle(color: Colors.white, fontSize: 30),)),
+                ),
+          ),
+          // second half of the login screen USER DETAILS
+          SizedBox(
+            height: height,
+            width: width * 0.5,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(mode),
+                const SizedBox(height: 40,),
+                const Text("username"),
+                const SizedBox(width: 10,),
+                SizedBox(
+                  width: 200,
+                  child: TextFormField(
+                    controller: username,
+                  ),
+                ),
+                const SizedBox(width: 20,),
+                const Text("password"),
+                const SizedBox(height: 10,),
+                SizedBox(
+                  width: 200,
+                  child: TextFormField(
+                    controller: password,
+                  ),
+                ),
+                const SizedBox(height: 20,),
+                ElevatedButton(
+                    onPressed: () {
+                      print(username.text);
+                      print(password.text);
+                    },
+                    child: Text("confirm")),
+                const SizedBox(height: 20,),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      if (mode == "Login") {
+                        mode = "Sign Up";
+                      }
+                      else {
+                        mode = "Login";
+                      }
+                    });
+                  },
+                  child: Text(getButtonText()),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
