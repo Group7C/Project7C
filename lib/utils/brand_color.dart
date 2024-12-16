bd019da3375fadee59550f831c2b4f10536dfb19
LagFactory <92254411+LagFactory@users.noreply.github.com>
2024-12-16 11:46:45 +0000

    Delete lib directory

--- lib/utils/brand_color.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class BrandColor{
-  static Color ChatInputColor = Colors.white;
-  static Color PrimaryColor = Colors.purple;
-}
--- lib/utils/textfield_styles.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ThemeTextStyle{
-  static TextStyle loginTextFieldStyle = TextStyle(color: Colors.blueGrey);
-}
